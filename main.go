f99f9cb05f2640176542fec5daf0b9f9abbde561
Ramso-dev <rossijatime@gmail.com>
2017-08-09 14:05:54 +0200

    corserror

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ var grafanaUserPassword = "2QkNlf7C8DG2Qtrj"
 
 func InitEnvironmentVariables() {
 	if os.Getenv("PROD") == "true" {
-		fmt.Println("Using production environment.V2.09.08.2017")
+		fmt.Println("Using production environment.V2.09.08.2017.")
 		ServiceAccount = os.Getenv("SERVICE_ACCOUNT")
 		grafanaBaseURL = os.Getenv("GRAFANA_URL")
 		apiBaseURL = os.Getenv("PORTAL_API")
